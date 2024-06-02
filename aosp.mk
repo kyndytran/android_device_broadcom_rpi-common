212f3a4e6d9b22bf7d07045f79daf88e24a6a210
Kha Tran <khab1304690@gmail.com>
2024-06-02 17:51:18 +0700

    rpi-common: Initialize basic files to support Android 14
    This patch adds some required files to support Android build:
    
    * rpi.mk: Includes specific apps/settings to support for all RPI devices
    * aosp.mk: Includes AOSP apps/settings to support for all RPI devices
    * BoardConfigCommon.mk: CPU configuration and related configuration
      to support Android.
    
    New device support should include *.mk from rpi-common
    
    Signed-off-by: Kha Tran <khab1304690@gmail.com>

---
+++ aosp.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 KhaTran
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
+# Include required AOSP packges/apps/settings
+# TODO: Support 32-bit arch/app (Use: core_64_bit.mk instead)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Update when configuring for Android TV, Automotive
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+# Update when updating to newer Android
+PRODUCT_SHIPPING_API_LEVEL := 34
+
+# Dalvik heap configuration for board which has 4GB RAM or higher
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
