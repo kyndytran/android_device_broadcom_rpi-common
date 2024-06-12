70485462311259a0146a2bfe6d5115c185af0e8e
Kha Tran <khab1304690@gmail.com>
2024-06-13 02:34:14 +0700

    rpi-common: Support Wi-Fi feature
    This patch adds support Wi-Fi feature
    It's not working yet.
    
    Pairing: OK
    Connect: Can't yet
    
    Signed-off-by: Kha Tran <khab1304690@gmail.com>

--- permission.mk
+++ permission.mk
@@ -28,3 +28,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.output.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.output.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
